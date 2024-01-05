6f9b6beb737f92ba747f38baca9908f179434806
AlexCondor <alexguerron@gmail.com>
2024-01-04 19:42:58 -0500

    Vue: props/v-bind style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <h2>hola app</h2>
   <img src="https://i.pinimg.com/originals/d2/23/d9/d223d948ba7dd7c538a969d712d64bfe.gif" alt="logo">
-  <Contador titulo="A"/>
-  <Contador titulo="B"/>
+  <Contador titulo="A" />
+  <Contador titulo="B" v-bind:valor='10'/>
+  <Contador titulo="C" :valor='calculo()'/>
   <h2>chao app</h2>
 </template>
 
@@ -14,6 +15,11 @@ export default {
   name: 'App',
   components: {
     Contador
+  },
+  methods:{
+    calculo(){
+      return 5*5;
+    }
   }
 }
 </script>
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,23 +1,40 @@
 <template>
+    <h2>hola comp</h2>
     <h1>Contador {{titulo}}</h1>
-    <h1>{{count}} <sup>2</sup> = {{obtenerCuadradoComp}}</h1>
-    <h1>{{count}} <sup>2</sup> = {{obtenerCuadradoComp}}</h1>
-    {{obtenerCuadrado()}}
-    <h2>adios comp</h2>
+    <h3>{{count}} <sup>2</sup> = {{obtenerCuadradoComp}}</h3>
+    <button @click="btn_mas">+1</button>
+    <button @click="btn_menos">-1</button>
+    <h2>chao comp</h2>
 </template>
 
 <script>
 export default {
-    props:["titulo"],
+    //props:["titulo",'valor'], forma de declaracion 1
+    props:{
+        titulo:String, //declaracion 2 espesificar el tipo de dato
+        valor: {
+            type: Number,
+            required: true,
+            default:100,
+            validator(value){
+                return value>20
+            }
+        }
+    },
     data(){
         return{
-            count: 5,
+            count: this.valor,
         }
     },
     methods:{
         obtenerCuadrado(){
-           
             return this.count*this.count;
+        },
+        btn_mas(){
+             return this.count+=1;
+        },
+        btn_menos(){
+            this.count-=1;
         }
     },
     computed:{
@@ -29,6 +46,28 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+*{
+    background-color: rgba(123, 0, 255, 0.338);
+}
+
+button{
+    margin: o 10px;
+    padding: 5px 15px;
+    background-color: rgb(165, 43, 226);
+    border-radius: 5px;
+    border: 0.5px solid white;
+    color: white;
+    cursor: pointer;
+}
+button:hover{
+    background-color: rgb(0, 0, 145);
+}
+h1{
+    font-family: cursive;
+}
+h2{
+    color: goldenrod;
+}
 
 </style>
