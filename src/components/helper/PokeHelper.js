ff66125f846462755dbe7f4c0423c02aa0dab897
AlexCondor <alexguerron@gmail.com>
2024-01-13 11:21:22 -0500

    helper parte 1.2

--- src/components/helper/PokeHelper.js
+++ src/components/helper/PokeHelper.js
@@ -1,10 +1,22 @@
 const obtenerID=()=>{
-    const pokeId1=obtenerAleatorio(600)
-    const pokeId2=obtenerAleatorio(600)
-    const pokeId3=obtenerAleatorio(600)
-    const pokeId4=obtenerAleatorio(600)
-    const PokeArray = [pokeId1,pokeId2,pokeId3,pokeId4]
+    const pokeId1=obtenerAleatorio(600);
+    const pokeId2=obtenerAleatorio(600);
+    const pokeId3=obtenerAleatorio(600);
+    const pokeId4=obtenerAleatorio(600);
+    const pokeArray = [pokeId1,pokeId2,pokeId3,pokeId4];
+    obrenerPokemon(pokeArray);
 }
+const obrenerPokemon = (pokeArray) =>{
+    const n1 = consumirAPU(pokeArray[0]);
+    const n2 = consumirAPU(pokeArray[0]);
+    const n3 = consumirAPU(pokeArray[0]);
+    const n4 = consumirAPU(pokeArray[0]);
+
+}
+function consumirAPU(id){
+    return "nombre"
+}
+
 const obtenerAleatorio=(m)=>{
     return 1+Math.floor(Math.random()*m)
 }
